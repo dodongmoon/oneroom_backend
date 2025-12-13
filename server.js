1bb83c84d49dd7db426ab6d405437644c5cce487
yang <dongmoon>
2025-12-13 21:28:47 +0900

    Add health check route

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Server is running successfully!');
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
